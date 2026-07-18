3aa1da2001ed4c249bb23ac9ce6888a83ac1cdb9
miladzaim <milad.zaim.mz@gmail.com>
2026-07-18 09:38:34 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil((async()=>{
+  const keys = await caches.keys();
+  await Promise.all(keys.map(k => caches.delete(k)));
+  await self.registration.unregister();
+  const clients = await self.clients.matchAll({type:'window'});
+  clients.forEach(c => c.navigate(c.url));
+})()));
+self.addEventListener('fetch', () => {});
